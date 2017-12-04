bacfd40e496e9dc113c03b71753409082e513ee0
argaghulamahmad <argaghulamahmad@gmail.com>
2017-12-04 22:34:50 +0700

    Memperbaharui src 'company_profile/models.py'

--- company_profile/models.py
+++ company_profile/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class CompanyAccount(models.Model):
     company_name = models.CharField(max_length=27)
     company_industries = models.CharField(max_length=100)
-    company_website = models.TextField()
+    company_website = models.URLField()
     company_headquarters = models.CharField(max_length=100)
     company_year_founded = models.CharField(max_length=4)
     company_type = models.CharField(max_length=50)
