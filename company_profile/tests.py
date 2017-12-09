45a31e340b4c46e142f8aa47f1fc9372e2b549d9
argaghulamahmad <argaghulamahmad@gmail.com>
2017-12-09 13:22:22 +0700

    Memperbaharui test app 'company_profile'

--- company_profile/tests.py
+++ company_profile/tests.py
@@ -4,17 +4,19 @@ from .models import *
 
 class CompanyProfileAppTest(TestCase):
     def test_model_company_account(self):
-        new_company_account = CompanyAccount.objects.create(company_name="Konekin Enterprise",
-                                                            company_industries="Computer Software",
-                                                            company_website="https://konekin-enterprise.herokuapp.com/",
-                                                            company_headquarters="Kota Depok, Jawa Barat",
-                                                            company_year_founded="2017",
-                                                            company_type="Self-Employed",
-                                                            company_size="2-10 employees",
-                                                            company_specialties="Web App Development",
-                                                            company_description="This linkedin company page created to accomplish our college group ('https://gitlab.com/KelompokB10PPW2017') assignment.",
-                                                            company_logo="https://media.licdn.com/mpr/mpr/shrink_200_200/AAIAAwDGAAoAAQAAAAAAAArvAAAAJDM1OGUyZGRiLTcxMWUtNGQzZC1hNWY3LWJjZDlhM2ZhNzA3Ng.png",
-                                                            )
+        new_company_account = CompanyAccount.objects.create(
+            company_id="13597930",
+            company_name="Konekin Enterprise",
+            company_industries="Computer Software",
+            company_website="https://konekin-enterprise.herokuapp.com/",
+            company_headquarters="Kota Depok, Jawa Barat",
+            company_year_founded="2017",
+            company_type="Self-Employed",
+            company_size="2-10 employees",
+            company_specialties="Web App Development",
+            company_description="This linkedin company page created to accomplish our college group ('https://gitlab.com/KelompokB10PPW2017') assignment.",
+            company_logo="https://media.licdn.com/mpr/mpr/shrink_200_200/AAIAAwDGAAoAAQAAAAAAAArvAAAAJDM1OGUyZGRiLTcxMWUtNGQzZC1hNWY3LWJjZDlhM2ZhNzA3Ng.png",
+        )
 
         counting_all_company_account = CompanyAccount.objects.all().count()
         self.assertEqual(new_company_account.__str__(), "Konekin Enterprise")
