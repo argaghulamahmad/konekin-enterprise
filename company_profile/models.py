d51452aaa2bb8ac89caf7789c2974c68e4281306
argaghulamahmad <argaghulamahmad@gmail.com>
2017-12-09 13:23:19 +0700

    Memperbaharui models app 'company_profile'

--- company_profile/models.py
+++ company_profile/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class CompanyAccount(models.Model):
+    company_id = models.CharField(max_length=20)
     company_name = models.CharField(max_length=27)
     company_industries = models.CharField(max_length=100)
     company_website = models.URLField()
