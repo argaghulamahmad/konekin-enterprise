7fe458fa4e754d57a2e9d06ee00f7d5bf23a9166
argaghulamahmad <argaghulamahmad@gmail.com>
2017-12-10 16:49:36 +0700

    Collectstatic

---
+++ konekin_enterprise/static/js/company_profile.15be0202d753.js
@@ -0,0 +1,25 @@
+console.log($("#address").text());
+var address = $("#address").text();
+
+function initMap() {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            var propertiPeta = {
+                center: results[0].geometry.location,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+            var marker = new google.maps.Marker({
+                map: peta,
+                position: results[0].geometry.location
+            });
+        } else {
+            var html = '<div class="alert alert-danger text-center">' +
+                'Lokasi tidak ditemukan!' +
+                '</div>';
+            $("#googleMap").append(html)
+        }
+    });
+}
---
+++ konekin_enterprise/static/js/company_profile.js
@@ -0,0 +1,26 @@
+//google map api
+console.log($("#company-location").text());
+var address = $("#company-location").text();
+
+function initMap() {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            var propertiPeta = {
+                center: results[0].geometry.location,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+            var marker = new google.maps.Marker({
+                map: peta,
+                position: results[0].geometry.location
+            });
+        } else {
+            var html = '<div class="alert alert-danger text-center">' +
+                'Location Not Found!' +
+                '</div>';
+            $("#googleMap").append(html)
+        }
+    });
+}
