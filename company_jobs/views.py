9fb55ea009cbe145f48c25cbe1d26eedfb9b8663
argaghulamahmad <argaghulamahmad@gmail.com>
2017-12-12 19:26:12 +0700

    Do all needed things for others app

--- company_jobs/views.py
+++ company_jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from company_profile.models import CompanyAccount
 
 # Create your views here.
+
+response = {}
+def company_jobs(request, id):
+    selected_company = CompanyAccount.objects.get(company_id=id)
+
+    response['company_logo'] = selected_company.company_logo
+    response['is_company_jobs_html'] = True
