8642bd75acc871d6e077c87af362d967061219cc
Alya Putri <alyaputri99@gmail.com>
2017-12-13 07:17:18 +0700

    it works!!!!\

--- company_forum/urls.py
+++ company_forum/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import company_forum,add_forum
 
 urlpatterns = [
+    url(r'^add-forum/(?P<id>.*)/$', add_forum, name='add-forum'),
     url(r'^(?P<id>.*)/$', company_forum, name='company-forum'),
-    url(r'^add-forum/(?P<id>.*)/$', add_forum, name='add_forum'),
 
 ]
